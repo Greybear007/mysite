5a72881dc066efa893dc78e7f21436d92defb89b
Lhx <huiivws@qq.com>
2020-10-14 02:33:26 +0800

    网站首页

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+from django.contrib import admin
+from trips.views import hello_world, home
+
+urlpatterns = (
+	url(r'^$', home, name='home'),
+	url(r'^admin/', admin.site.urls),
+	url(r'^hello/$', hello_world, name='hello_world'),
+)
+
--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from datetime import datetime
+from trips.models import Post
+
+
+def hello_world(request):
+	context = {'current_time': datetime.now()}
+	return render(request, 'hello_world.html', context=context)
+
+def home(request):
+	post_list = Post.objects.filter(id__lte=10)
+	context = {'post_list': post_list}
+	return render(request, 'home.html', context=context)
+
